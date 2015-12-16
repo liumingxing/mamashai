b97fceaab30a658d55a80ee40a38730bb0f938f3
happyming <339755551@qq.com>
2015-12-16 15:21:21 +0800

    添加给伯克投票给晒豆功能。

--- app/models/bokergen_vote.rb
+++ app/models/bokergen_vote.rb
@@ -2,4 +2,5 @@ class BokergenVote < ActiveRecord::Base
   attr_accessible :name, :remark, :status, :vote_num, :user_id
   default_value_for :status, "stash"
   validates_uniqueness_of :name
+  belongs_to :user
 end
