cf88fe130cffc501419175e3688e6ca2816e800b
Dailin0811 <dailin20081028@163.com>
2015-12-17 14:26:04 +0800

    伯克给豆跳转问题

--- app/controllers/mms/bokergen_vote_controller.rb
+++ app/controllers/mms/bokergen_vote_controller.rb
@@ -8,7 +8,7 @@ class Mms::BokergenVoteController < Mms::MmsBackEndController
       redirect_to(action: :index, alert: '已经给过豆豆') and return if bokergen_vote.status == 'processed'
       Mms::Score.trigger_event(:make_bokergen_vote, "给伯克生物投票", 50, 1, {:user => bokergen_vote.user})
       bokergen_vote.update_attribute(:status, 'processed')
-      redirect_to action: :index, notice: '操作成功。'
+      redirect_to params[:back], notice: '操作成功。'
     end
 
 end
